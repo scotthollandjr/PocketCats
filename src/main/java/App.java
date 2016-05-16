f519fecaa1aceb6ffe4641a71ecb6545c478fd80
John Dow and Sam Durfey <sam@>
2016-05-16 11:28:09 -0700

    creating sidebar

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,6 +19,12 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/cats", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/cats.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
 
