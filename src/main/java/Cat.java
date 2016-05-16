f1e605fdef517c291def62ba3b2c26a0347dbdab
John Dow, Lilo Onwuzu, Sam Durfey and Scott Holland <samuel.durfey@gmail.com>
2016-05-16 10:28:57 -0700

    Add many to many methods and association for Cat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TimeandDate;
+import org.sql2o.*;
 
 public class Cat {
   private int id;
@@ -38,6 +42,48 @@ public class Cat {
 
   public getDescription() {
     return description;
+  }
+
+  // Cat has many to many with users
+  public void addUser(User user){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO cats_users(cat_id, user_id) VALUES (:cat_id, :user_id)";
+      con.createQuery(sql)
+        .addParameter("cat_id", this.getId())
+        .addParameter("user_id", user.getId())
+        .executeUpdate();
+    }
+  }
+
+  // Cat has many to many with usersv
+  public List<User> getUsers() {
+    String sql = "SELECT user_id FROM cats_users WHERE cat_id=:cat_id";
+    try(Connection con = DB.sql2o.open()) {
+      List<Integer> userIds = con.createQuery(sql)
+        .addParameter("cat_id", this.getId())
+        .executeAndFetch(Integer.class);
+
+      List<User> users = new ArrayList<User>()
+      for (Integer userId : userIds) {
+        String sql = "SELECT * FROM users WHERE id=:id";
+        User newUser = con.createQuery(sql) {
+          .addParameter("id", userId)
+          executeAndFetch(User.class);
+          users.add(newUser);
+        }
+        return users;
+      }
+    }
+  }
+
+  // Cat has one to many with Comments
+  public List<Comments> getComments() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM comments WHERE cat_id=:cat_id";
+      return con.createQuery(sql)
+        .addParameter("cat_id", this.getId())
+        .executeAndFetch(Comment.class);
+    }
   }
 
 }
