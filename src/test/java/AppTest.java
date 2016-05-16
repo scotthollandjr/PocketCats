3b40196322bb4489d84c54cf70ca369f249ae062
John Dow, Lilo Onwuzu, Sam Durfey and Scott Holland <jldjld503@gmail.com>
2016-05-16 15:23:42 -0700

    Temporily comment out unused files

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,28 +1,28 @@
-import org.fluentlenium.adapter.FluentTest;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import spark.ModelAndView;
-import static org.fluentlenium.core.filter.FilterConstructor.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class AppTest extends FluentTest {
-  public WebDriver webDriver = new HtmlUnitDriver();
-
-  @Override
-  public WebDriver getDefaultDriver() {
-    return webDriver;
-  }
-
-  @ClassRule
-  public static ServerRule server = new ServerRule();
-
-  @Rule
-  public ClearRule clearRule = new ClearRule();
-
-}
+// import org.fluentlenium.adapter.FluentTest;
+// import org.junit.ClassRule;
+// import org.junit.Test;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+// import java.util.ArrayList;
+// import java.util.Map;
+// import java.util.HashMap;
+// import spark.ModelAndView;
+// import static org.fluentlenium.core.filter.FilterConstructor.*;
+//
+// import static org.assertj.core.api.Assertions.assertThat;
+//
+// public class AppTest extends FluentTest {
+//   public WebDriver webDriver = new HtmlUnitDriver();
+//
+//   @Override
+//   public WebDriver getDefaultDriver() {
+//     return webDriver;
+//   }
+//
+//   @ClassRule
+//   public static ServerRule server = new ServerRule();
+//
+//   @Rule
+//   public ClearRule clearRule = new ClearRule();
+//
+// }
