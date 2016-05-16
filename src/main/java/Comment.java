3621a023a4ef80a300c0700d84e4a0fbbfe44076
John Dow, Lilo Onwuzu, Sam Durfey and Scott Holland <scotthollandjr@gmail.com>
2016-05-16 16:43:18 -0700

    Add methods with passing tests

--- src/main/java/Comment.java
+++ src/main/java/Comment.java
@@ -1,6 +1,81 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import org.sql2o.*;
+
 public class Comment {
+  private int id;
+  private String description;
+  private LocalDateTime date;
+  private int user_id;
+  private int cat_id;
 
-  public Comment() {
-
+  public Comment(String description) {
+    this.description = description;
+    this.date = date;
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public LocalDateTime getDate() {
+    return date;
+  }
+
+  public static List<Comment> all() {
+    String sql = "SELECT id, user_id, description, date FROM comments";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+        .executeAndFetch(Comment.class);
+    }
+  }
+
+  @Override
+  public boolean equals (Object otherComment) {
+    if(!(otherComment instanceof Comment)) {
+      return false;
+    } else {
+      Comment newComment = (Comment) otherComment;
+      return this.getDescription().equals(newComment.getDescription()) &&
+             this.getId() == newComment.getId();
+    }
+  }
+
+
+  public void save() {
+    String sql = "INSERT INTO comments (user_id, description, date) VALUES (:user_id, :description, :date)";
+    try (Connection con = DB.sql2o.open()) {
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("user_id", this.getId()) //what do i put here!
+        .addParameter("description", this.getDescription())
+        .addParameter("date", this.getDate())
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Comment find(int id) {
+    String sql = "SELECT * FROM comments WHERE id=:id";
+    try (Connection con = DB.sql2o.open()) {
+      Comment comment = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Comment.class);
+      return comment;
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String deleteQuery = "DELETE FROM comments WHERE id=:id";
+      con.createQuery(deleteQuery)
+        .addParameter("id", this.getId())
+        .executeUpdate();
+    }
+  }
+
 }
---
+++ src/test/java/CommentTest.java
@@ -0,0 +1,60 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+public class CommentTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Comment_instantiatesCorrectly_true() {
+    Comment newComment = new Comment("This cat rules!");
+    assertEquals(true, newComment instanceof Comment);
+  }
+
+  @Test
+  public void all_EmptyAtFirst_true() {
+    assertEquals(0, Comment.all().size());
+  }
+
+  @Test
+  public void equals_returnTrueIdCommentsAreTheSame() {
+    Comment newComment = new Comment("This cat rules!");
+    Comment newerComment = new Comment("This cat rules!");
+    assertTrue(newComment.equals(newerComment));
+  }
+
+  @Test
+  public void save_savesToCommentDatabase() {
+    Comment newComment = new Comment("This cat rules!");
+    newComment.save();
+    assertTrue(Comment.all().get(0).equals(newComment));
+  }
+
+  @Test
+  public void save_saveAssignsIdToObject() {
+    Comment newComment = new Comment("This cat rules!");
+    newComment.save();
+    assertEquals(newComment.getId(), Comment.all().get(0).getId());
+  }
+
+  @Test
+  public void find_findCommentInDatabase() {
+    Comment newComment = new Comment("This cat rules!");
+    newComment.save();
+    Comment savedComment = Comment.all().get(0);
+    assertEquals(savedComment, Comment.find(newComment.getId()));
+  }
+
+  @Test
+  public void delete_deletesObject() {
+    Comment newComment = new Comment("This cat rules!");
+    newComment.save();
+    newComment.delete();
+    assertEquals(0, Comment.all().size());
+  }
+}
