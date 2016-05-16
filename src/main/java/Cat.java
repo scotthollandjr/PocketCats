2281897e72c591c27aa28acdb0fdc342d03d05c5
John Dow, Lilo Onwuzu, Sam Durfey and Scott Holland <scotthollandjr@gmail.com>
2016-05-16 09:55:01 -0700

    Add basic Cat methods

---
+++ src/main/java/Cat.java
@@ -0,0 +1,43 @@
+
+public class Cat {
+  private int id;
+  private String name;
+  private Boolean status;
+  private String location;
+  private Timestamp date;
+  private String description;
+
+  public Cat() {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+    this.location = location;
+    this.date = date;
+    this.description = description;
+  }
+
+  public getId() {
+    return id;
+  }
+
+  public getName() {
+    return name;
+  }
+
+  public getStatus() {
+    return status;
+  }
+
+  public getLocation() {
+    return location;
+  }
+
+  public getDate() {
+    return date;
+  }
+
+  public getDescription() {
+    return description;
+  }
+
+}
